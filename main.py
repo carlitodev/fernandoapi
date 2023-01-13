ec070332d82f29fc57feb1a91ab6330fc5a395f8
Carlos Oliveira13 <carlos.oliveira@thoughtworks.com>
2023-01-13 19:15:43 -0300

    Add first comit

---
+++ main.py
@@ -0,0 +1,34 @@
+from typing import Union, List
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Person(BaseModel):
+    id: int
+    name: str
+    age: int
+
+
+DB: List[Person] = [
+    Person(id=1, name="Jamila", age=25),
+    Person(id=2, name="Gabriela", age=31),
+    Person(id=3, name="Fernando", age=22),
+]
+
+
+@app.get("/api")
+def read_api():
+    return DB
+
+
+@app.get("/")
+def read_root ():
+    return {"Hello": "Fernando"}
+
+
+@app.get("/items/{item_id}")
+def read_item (item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
